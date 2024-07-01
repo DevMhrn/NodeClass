e793e6d081b5f5079d9f61eb9ae91aa0ff150114
Debashis Maharana <debashismaharana7854@gmail.com>
2024-07-01 11:51:23 +0530

    Read the html file

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    if(req.url === '/login'){
-        res.write('<html> <head> <title> Login Page</title> </head> <body> <h1>Login Page</h1> </body> </html>')
-        
-    }
-    else{
+    if (req.url === '/login') {
+        const filepath = path.join(__dirname, 'index.html');
+        try {
+            const content = fs.readFileSync(filepath, 'utf8');
+            res.write(content);
+        } catch (err) {
+            console.error(err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Internal Server Error');
+        }
+    } else {
         res.write('<html> <head> <title>My First Page</title> </head> <body> <h1>Hello World!</h1> </body> </html>');
-        
-
     }
     res.end();
-    
-})
+});
+
 const port = 3002;
 const host = 'localhost';
 
-server.listen(port, host, ()=>{
+server.listen(port, host, () => {
     console.log(`Server is running at http://${host}:${port}`);
 });
-
-
-//create a server 
