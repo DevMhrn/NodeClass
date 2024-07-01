518e99457b19c79959b03522343449501aabbbc9
Debashis Maharana <debashismaharana7854@gmail.com>
2024-07-01 11:44:31 +0530

    made the login page

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html> <head> <title>My First Page</title> </head> <body> <h1>Hello World!</h1> </body> </html>');
+    if(req.url === '/login'){
+        res.write('<html> <head> <title> Login Page</title> </head> <body> <h1>Login Page</h1> </body> </html>')
+        
+    }
+    else{
+        res.write('<html> <head> <title>My First Page</title> </head> <body> <h1>Hello World!</h1> </body> </html>');
+        
+
+    }
     res.end();
+    
 })
 const port = 3002;
 const host = 'localhost';
@@ -12,5 +21,6 @@ const host = 'localhost';
 server.listen(port, host, ()=>{
     console.log(`Server is running at http://${host}:${port}`);
 });
+
 
 //create a server 
